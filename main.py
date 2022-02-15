9e8e4d084a403a8a0e28d3b754850408a9871bb4
artyomzhibarev <artyom.zhibarev@yandex.ru>
2022-02-15 11:26:24 +0300

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from timeit import repeat
 from dis import dis
 
 
@@ -95,13 +96,73 @@ class RingBufferOverflow(RingBuffer):
         if self.head == self.tail:
             self.is_full = True
 
+
 # 3. На языке Python реализовать функцию, которая быстрее всего (по процессорным тикам) отсортирует данный ей массив
 # чисел. Массив может быть любого размера со случайным порядком чисел (в том числе и отсортированным). Объяснить
 # почему вы считаете, что функция соответствует заданным критериям.
+# Ответ:
+# Используя функцию, run_sorting_algorithm измерим время выполнения сортировки для алгоритмов bubble_sort, quick_sort и
+# встроенную функцию сортировки sorted.
+
+# Algorithm: sorted. Minimum execution time: 0.0006867080000000012
+# Algorithm: bubble_sort. Minimum execution time: 0.6924741669999999
+# Algorithm: quick_sort. Minimum execution time: 0.023409542000000005
+# Как видно, самые лучшие результаты показывает стандартная функция сортировки
+# Вывод: для сортировки чисел, лучше использовать стандартную функцию сортировки
+
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+
+def quick_sort(arr):
+    elements = len(arr)
+
+    # Base case
+    if elements < 2:
+        return arr
+
+    current_position = 0  # Position of the partitioning element
+
+    for i in range(1, elements):  # Partitioning loop
+        if arr[i] <= arr[0]:
+            current_position += 1
+            temp = arr[i]
+            arr[i] = arr[current_position]
+            arr[current_position] = temp
+
+    temp = arr[0]
+    arr[0] = arr[current_position]
+    arr[current_position] = temp  # Brings pivot to it's appropriate position
+
+    left = quick_sort(arr[0:current_position])  # Sorts the elements to the left of pivot
+    right = quick_sort(arr[current_position + 1:elements])  # sorts the elements to the right of pivot
+
+    arr = left + [arr[current_position]] + right  # Merging everything together
+
+    return arr
+
+
+def run_sorting_algorithm(algorithm, array):
+
+    setup_code = f"from __main__ import {algorithm}" if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Выполнить код десять раз и каждый раз
+    # вернуть время в секундах
+    times = repeat(setup=setup_code, stmt=stmt, repeat=5, number=10)
+
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
+    return min(times)
 
 
 if __name__ == '__main__':
-    buffer = RingBufferOverflow(buffer_size=4)
+    # buffer = RingBufferOverflow(buffer_size=4)
     # for i in range(9):
     #     buffer.put_element(randint(0, 100))
     #     print(buffer)
@@ -109,6 +170,13 @@ if __name__ == '__main__':
     # for i in range(10):
     #     buffer.put_element(randint(0, 100))
     #     print(buffer)
-    print(dis(is_even))
-    print(dis(is_even_bitwise))
-    # print(is_even_bitwise(20))
+    # print(dis(is_even))
+    # print(dis(is_even_bitwise))
+
+    array = [randint(0, 100) for i in range(1000)]
+    # Вызвать функцию, используя имя алгоритма сортировки
+    # и массив, который вы только что создали, передать ей
+    t1 = run_sorting_algorithm(algorithm="sorted", array=array)
+    t2 = run_sorting_algorithm(algorithm="bubble_sort", array=array)
+    t3 = run_sorting_algorithm(algorithm="quick_sort", array=array)
+
